ce2da4012b7b0a8926f945ebd55e56cc87933ada
David <ashuaib1991@gmail.com>
2025-08-05 10:18:08 +0100

    fix: bind server to 0.0.0.0 for Fly

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ app.use((err, req, res, next) => {
 
     await connectDB();
     const PORT = process.env.PORT || 8080;
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
+
   } catch (err) {
     console.error("❌ Failed to start app:", err.message);
     process.exit(1);
